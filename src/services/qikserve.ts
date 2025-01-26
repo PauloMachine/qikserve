13c643a0598c0b84dd812a6f26a90af41bba58ac
Paulo Guilherme Santiago <37718188+PauloMachine@users.noreply.github.com>
2025-01-26 16:13:02 -0300

    refactor: adjust base url

--- src/services/qikserve.ts
+++ src/services/qikserve.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_OLIN_API_URL,
+  baseURL: process.env.REACT_APP_QIKSERVE_URL,
   headers: {
     "Referrer-Policy": "strict-origin-when-cross-origin",
     "X-Requested-With": "XMLHttpRequest",
